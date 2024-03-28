b31e25ed43982d9db3a627b75cd77690370f265b
wambui-g <grayciekamau03@gmail.com>
2024-03-28 19:52:54 +0300

    update

---
+++ styles/script.js
@@ -0,0 +1,24 @@
+document.getElementById('showCard1').addEventListener('click', function() {
+    document.getElementById('cardContainer1').style.display = 'block';
+  });
+  
+  function closeCard1() {
+    document.getElementById('cardContainer1').style.display = 'none';
+  }
+
+  
+  document.getElementById('showCard2').addEventListener('click', function() {
+    document.getElementById('cardContainer2').style.display = 'block';
+  });
+  function closeCard2() {
+    document.getElementById('cardContainer2').style.display = 'none';
+  }
+
+  
+  document.getElementById('showCard3').addEventListener('click', function() {
+    document.getElementById('cardContainer3').style.display = 'block';
+  });
+  function closeCard3() {
+    document.getElementById('cardContainer3').style.display = 'none';
+  }
+  
